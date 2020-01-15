3a764c19a194279cc544798230c407cec5c61cad
daniel love <danlove99@hotmail.co.uk>
2020-01-15 10:48:09 +0000

    Initial commit

---
+++ SKSpotify.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+
+
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv('top50.csv', encoding = "ISO-8859-1")
+
+X = data.drop(['ID', 'Track.Name', 'Artist.Name', 'Genre', 'Loudness..dB..', 'Popularity'], axis=1)
+y = data['Popularity']
+# print(X.head())
+print(X.shape)
+# print(y.head())
+print(y.shape)
+
+knn = KNeighborsClassifier(n_neighbors=12)
+knn.fit(X, y)
+
+# make a prediction for an example of an out-of-sample observation
+knn.predict([[95, 57, 73, 11, 5, 182, 9, 16]])
+
+# With logistic regression
+#logreg = LogisticRegression()
+#logreg.fit(X_train, y_train)
+#y_pred = logreg.predict(X_test)
+#print(metrics.accuracy_score(y_test, y_pred))
